9e348c393380735655876003b2441f5bab098478
Jake Lamb <jacobtaylorlamb@gmail.com>
2020-01-25 00:10:27 -0700

    update gatsby-config

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -12,26 +12,11 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },/*
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [],
-      },
-    },*/
+    },
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: "https://lambandco-cms.herokuapp.com/", //http://localhost:1337
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
